9b6b16e4162d6ff0e09531f3f3d9ffc0b54ef2de
Axel Koolhaas <axel.koolhaas@hotmail.com>
2023-10-19 16:01:59 +0200

    Improved ShutdownHandler.

--- src/main/java/org/springframework/samples/petclinic/util/ShutdownHandler.java
+++ src/main/java/org/springframework/samples/petclinic/util/ShutdownHandler.java
@@ -7,25 +7,48 @@ import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Component;
 
+/**
+ * A utility class for shutting down a Spring Boot application.
+ * <a href="https://github.com/spring-projects/spring-boot/blob/main/spring-boot-project/spring-boot-actuator/src/main/java/org/springframework/boot/actuate/context/ShutdownEndpoint.java">Based on actuator</a>
+ */
 @Component
 public class ShutdownHandler implements ApplicationContextAware {
 
     private ConfigurableApplicationContext context;
+    private int exitCode = 0;
 
     @Override
     public void setApplicationContext(@NonNull ApplicationContext context) throws BeansException {
-        this.context = (ConfigurableApplicationContext) context;
+        if (context instanceof ConfigurableApplicationContext configurableContext) {
+            this.context = configurableContext;
+        }
     }
 
-    public void terminate(int exitCode) {
-        validateExitCode(exitCode);
-        context.close();
-        System.exit(exitCode);
+    public void shutdown(int exitCode) {
+        setExitCode(exitCode);
+        Thread thread = new Thread(this::performShutdown);
+        thread.setContextClassLoader(getClass().getClassLoader());
+        thread.start();
     }
 
-    private void validateExitCode(int exitCode) {
+    private void setExitCode(int exitCode) {
         if (exitCode < 0 || exitCode > 255) {
             throw new IllegalArgumentException("Exit code must be between 0 and 255.");
         }
+        this.exitCode = exitCode;
+    }
+
+    private void performShutdown() {
+        // Delay to allow clients to receive HTTP response
+        try {
+            Thread.sleep(500L);
+        }
+        catch (InterruptedException ex) {
+            Thread.currentThread().interrupt();
+        }
+        // Perform actual shutdown
+        this.context.close();
+        // (Force) kill the JVM to set the exit code
+        System.exit(this.exitCode);
     }
 }
